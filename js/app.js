c4f3f94cb714e4ad386b2acd512fd0ce254da0e1
Michelle Garcia <garcia_michelle@att.net>
2018-06-10 22:37:12 -0500

    Randomized order of cards on page load.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-var card = document.querySelectorAll('.card');
+var parentContainer = document.getElementById('game-container');
+var cards = document.querySelectorAll('.card');
 
-for (var i = 0; i < card.length; i++) {
-    
-    card[i].addEventListener('click', function () {
+for (var i = parentContainer.children.length; i >= 0; i--) {
+    parentContainer.appendChild(parentContainer.children[Math.random() * i | 0]);
+}
+ 
+for (var j = 0; j < cards.length; j++) {
+    cards[j].addEventListener('click', function () {
         icon = this.firstChild;
         icon.classList.toggle('hide');
     });
-}
+};
+ 
