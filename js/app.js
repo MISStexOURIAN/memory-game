05e9fd17e10fc29be9b976cc06d45217f89cf0e1
Michelle Garcia <garcia_michelle@att.net>
2018-06-18 12:54:01 -0500

    Two unmatching cards turn back over after a 2 second delay.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 var parentContainer = document.getElementById('game-container');
 var cards = document.querySelectorAll('.card');
 var countClicks = 0;
-var cardValue;
-var player1Matches = document.getElementById('player1Matches').innerHTML;
-var player2Matches = document.getElementById('player2Matches').innerHTML;
+var clickedCardsIcons = [];
+var clickedCardsClass = [];
+var activePlayer = document.getElementsByClassName('underline');
+
+
+
 
 for (var i = parentContainer.children.length; i >= 0; i--) {
     parentContainer.appendChild(parentContainer.children[Math.random() * i | 0]);
@@ -13,22 +16,43 @@ for (var j = 0; j < cards.length; j++) {
     cards[j].addEventListener('click', function () {
         icon = this.firstChild;
         icon.classList.toggle('hide');
-        cardValue = this.value;
-        countClicks += 1;
-        if (countClicks == 2) {
+        countClicks += 1;        
+        clickedCardsIcons.push(icon);
+        clickedCardsClass.push(this.className);
+
+        if (countClicks === 2) {
             countClicks = 0;
             var player1 = document.getElementById('playerOne');
             var player2 = document.getElementById('playerTwo');
             player1.classList.toggle('underline');
             player2.classList.toggle('underline');
-            if (cardValue == cardValue) {
+            var firstCard = clickedCardsIcons[0];
+            var secondCard = clickedCardsIcons[1];
+    
+
+            if (clickedCardsClass[0] == clickedCardsClass[1]) {
+                // var matches = activePlayer.nextSibling.firstChild.innerHTML;
+                // matches += 1;
+            } else {
+                setTimeout(function (icon) {
+                    
+                    firstCard.classList.toggle('hide');
+                    secondCard.classList.toggle('hide');
+                }, 2000);
+            }
+
+            clickedCardsClass = [];
+            clickedCardsIcons = [];
+
+            
+            
                 // add one to 'matches' for the current player
                 
                 // keep cards turned over
-            } else {
+        
                 // turn cards back over
                 
-            }
-        }
+    
+        };
     });
 };    
