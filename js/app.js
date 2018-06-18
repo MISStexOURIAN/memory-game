ed216c99e68fe8827517ca711d2a894715136e13
Michelle Garcia <garcia_michelle@att.net>
2018-06-18 10:34:41 -0500

    Active player changes after two card clicks.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 var parentContainer = document.getElementById('game-container');
 var cards = document.querySelectorAll('.card');
+var countClicks = 0;
+var cardValue;
+var player1Matches = document.getElementById('player1Matches').innerHTML;
+var player2Matches = document.getElementById('player2Matches').innerHTML;
 
 for (var i = parentContainer.children.length; i >= 0; i--) {
     parentContainer.appendChild(parentContainer.children[Math.random() * i | 0]);
@@ -9,6 +13,22 @@ for (var j = 0; j < cards.length; j++) {
     cards[j].addEventListener('click', function () {
         icon = this.firstChild;
         icon.classList.toggle('hide');
+        cardValue = this.value;
+        countClicks += 1;
+        if (countClicks == 2) {
+            countClicks = 0;
+            var player1 = document.getElementById('playerOne');
+            var player2 = document.getElementById('playerTwo');
+            player1.classList.toggle('underline');
+            player2.classList.toggle('underline');
+            if (cardValue == cardValue) {
+                // add one to 'matches' for the current player
+                
+                // keep cards turned over
+            } else {
+                // turn cards back over
+                
+            }
+        }
     });
-};
- 
+};    
