9d8645fbb39775eb90fa229f0947c2263a1327fd
Michelle Garcia <garcia_michelle@att.net>
2018-06-18 16:46:42 -0500

    Number of matches increases by 1 for current player if two matching cards are clicked.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 var parentContainer = document.getElementById('game-container');
 var cards = document.querySelectorAll('.card');
 var countClicks = 0;
+var clickedCards = [];
 var clickedCardsIcons = [];
 var clickedCardsClass = [];
-var activePlayer = document.getElementsByClassName('underline');
-
+var matches = 0;
+var player1Matches = document.getElementById('player1Matches');
+var player2Matches = document.getElementById('player2Matches');
 
 
 
@@ -14,6 +16,7 @@ for (var i = parentContainer.children.length; i >= 0; i--) {
  
 for (var j = 0; j < cards.length; j++) {
     cards[j].addEventListener('click', function () {
+        clickedCards.push(this);
         icon = this.firstChild;
         icon.classList.toggle('hide');
         countClicks += 1;        
@@ -22,35 +25,40 @@ for (var j = 0; j < cards.length; j++) {
 
         if (countClicks === 2) {
             countClicks = 0;
-            var player1 = document.getElementById('playerOne');
-            var player2 = document.getElementById('playerTwo');
-            player1.classList.toggle('underline');
-            player2.classList.toggle('underline');
+
+            setTimeout(function () {
+                var player1 = document.getElementById('playerOne');
+                var player2 = document.getElementById('playerTwo');
+                player1.classList.toggle('underline');
+                player2.classList.toggle('underline');
+            }, 1200);
+            
             var firstCard = clickedCardsIcons[0];
             var secondCard = clickedCardsIcons[1];
     
 
             if (clickedCardsClass[0] == clickedCardsClass[1]) {
-                // var matches = activePlayer.nextSibling.firstChild.innerHTML;
-                // matches += 1;
+                var player1 = document.getElementById('playerOne');
+                var player2 = document.getElementById('playerTwo');
+                if (player1.classList.contains('underline')) {
+                    var newScore = parseInt(player1Matches.innerText) + 1;
+                    player1Matches.innerText = newScore;
+                } else if (player2.classList.contains('underline')) {
+                    var newScore = parseInt(player2Matches.innerText) + 1;
+                    player2Matches.innerText = newScore;
+                }
+                
+
             } else {
-                setTimeout(function (icon) {
-                    
+                setTimeout(function (icon) {                    
                     firstCard.classList.toggle('hide');
                     secondCard.classList.toggle('hide');
-                }, 2000);
+                }, 1200);
             }
 
             clickedCardsClass = [];
-            clickedCardsIcons = [];
-
-            
-            
-                // add one to 'matches' for the current player
-                
-                // keep cards turned over
-        
-                // turn cards back over
+            clickedCardsIcons = []; 
+            clickedCards = [];
                 
     
         };
