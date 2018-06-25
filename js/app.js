7877ec528fbbded4f11ed09ccb579cd55ecb916f
Michelle Garcia <garcia_michelle@att.net>
2018-06-25 11:12:54 -0500

    Disable click event on already-selected cards and matched cards, and enable click event after cards turn back over.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 {
     let cards = document.querySelectorAll('.card');
     let cardClicks = 0;
-    let clickedCardClass = [];
-    let clickedCard = [];
+    let clickedCardClass = []; // array of classes of cards clicked
+    let clickedIcon = []; // array of icons that have been clicked
+    let clickedCard = []; // array of card divs that have been clicked
     let matches = 0;
     let guesses = 0;
     let icon;
     var parentContainer = document.getElementById('game-container');
     let timerVar = setInterval(countTimer, 1000);
     let totalSeconds = 0;
+
 
     // randomize cards and start timer on page load
     randomizeCards();
@@ -47,21 +49,26 @@
     }
 
     // function to run when cards don't match
-    function noMatch() {
-        let firstCard = clickedCard[0];
-        let secondCard = clickedCard[1];
+    function noMatch(clickedCard) {
+        let firstCardIcon = clickedIcon[0];
+        let secondCardIcon = clickedIcon[1];
+
         // turn over unmatching cards after delay
-        setTimeout(function() {
-            firstCard.classList.toggle('hide');
-            secondCard.classList.toggle('hide');
-        }, 1200);
+        setTimeout(function () {
+            firstCardIcon.classList.toggle('hide');
+            secondCardIcon.classList.toggle('hide');   
+            clickedCard.forEach(function (card) {
+                card.addEventListener('click', clickEvent);
+            });
+        }, 1500);
     }
 
     function resetItems() {
         clickedCardClass = []; // reset icon class array to empty
-        clickedCard = []; // reset clicked cards array to empty
+        clickedIcon = []; // reset clicked cards array to empty
         guesses += 1; // increase number of guesses by 1
-        document.getElementById('guesses').innerHTML = guesses;
+        document.getElementById('guesses').innerHTML = guesses;  
+        clickedCard = [];
     }
 
     function stars() { // decrease star rating according to total guesses
@@ -74,31 +81,40 @@
         };
     }
 
-    for (let j = 0; j < cards.length; j++) { // attach click event to cards
-        cards[j].addEventListener('click', function () {
-            icon = this.firstChild;
-            icon.classList.toggle('hide'); // turn card over
-            clickedCardClass.push(this.className); // store card class to test for matching
-            clickedCard.push(icon); // store card that was clicked to enable hiding in noMatch function
-            cardClicks++; // track number of clicks
+    function clickEvent() {
+        icon = this.firstChild;
+        icon.classList.toggle('hide'); // turn card over
+        clickedCard.push(this);
+        clickedCardClass.push(this.className); // store card class to test for matching
+        clickedIcon.push(icon); // store card that was clicked to enable hiding in noMatch function
+        cardClicks++; // track number of clicks
 
-            if (cardClicks === 2) { // when two cards are clicked      
-    
-                if (clickedCardClass[0] != clickedCardClass[1]) { // if the icons don't match
-                    noMatch(); // run the noMatch function
-                    resetItems();        
-                } else { // if cards match
-                    matches += 1; // increase number of matches by 1
-                    resetItems();
-                    if (matches === 8) { // if all cards have been matched
-                        win(); // run the win function
-                    };
+        if (cardClicks) {
+            clickedCard.forEach(function (card) {
+                card.removeEventListener('click', clickEvent);
+            });
+        }
+
+        if (cardClicks === 2) { // when two cards are clicked  
+            if (clickedCardClass[0] != clickedCardClass[1]) { // if the icons don't match
+                noMatch(clickedCard); // run the noMatch function
+                resetItems();      
+            } else { // if cards match
+                // remove these cards from the cards array to disable functionality
+                matches += 1; // increase number of matches by 1
+                resetItems();
+                if (matches === 8) { // if all cards have been matched
+                    win(); // run the win function
                 };
-
-                cardClicks = 0; // reset card clicks counter to 0
             };
 
-            stars(); // change star rating accoring to number of guesses
-        });    
+            cardClicks = 0; // reset card clicks counter to 0
+        };
+
+        stars(); // change star rating accoring to number of guesses
+    }
+
+    for (let j = 0; j < cards.length; j++) { // attach click event to cards
+        cards[j].addEventListener('click', clickEvent);    
     };    
 }
